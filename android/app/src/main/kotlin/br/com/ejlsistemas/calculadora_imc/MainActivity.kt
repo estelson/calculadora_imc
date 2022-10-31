bd85aed540297deb4d62fc0eb3ac16dd54a01fc8
Estelson Medeiros Pereira <estelson@gmail.com>
2022-10-31 20:16:35 -0300

    Initial commit
    Projeto do curso Udemy - Criação de Apps Android/iOS/Web com Flutter - 5 cursos em 1 - seção 16 - Cálculo de IMC

---
+++ android/app/src/main/kotlin/br/com/ejlsistemas/calculadora_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ejlsistemas.calculadora_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+      ),
+    );
+  }
+
+}
